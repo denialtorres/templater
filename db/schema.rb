0b003c07bc95caa047061175d1fce12b887521be
denialtorres <denial.torres@gmail.com>
2025-01-25 11:22:15 -0600

    user scaffolding, render the users/index view from the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_25_024954) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_25_170556) do
   create_table "sql_templates", force: :cascade do |t|
     t.text "body"
     t.string "path"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_25_024954) do
     t.string "locale"
     t.string "handler"
     t.boolean "partial", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
